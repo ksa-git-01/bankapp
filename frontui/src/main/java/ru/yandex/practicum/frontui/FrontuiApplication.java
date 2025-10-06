af91765c2476ec1c91658b7c5d4db5c01a2622e0
ksa <krutikov.s@gmail.com>
2025-10-06 12:39:01 +0300

    Root: init FrontUI module

---
+++ frontui/src/main/java/ru/yandex/practicum/frontui/FrontuiApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.frontui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontuiApplication.class, args);
+	}
+
+}
