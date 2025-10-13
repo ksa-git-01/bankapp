823dfdfac70716d90cb317cd7cbf1ac0a6a10242
ksa <krutikov.s@gmail.com>
2025-10-13 15:11:33 +0300

    FrontUI: Add registration exception

---
+++ frontui/src/main/java/ru/yandex/practicum/frontui/exception/RegistrationException.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.frontui.exception;
+
+public class RegistrationException extends RuntimeException {
+    public RegistrationException(String message) {
+        super(message);
+    }
+}
