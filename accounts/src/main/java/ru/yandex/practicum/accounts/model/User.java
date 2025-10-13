a4c36b71a9ea737152ad9c3e77478fc420561901
ksa <krutikov.s@gmail.com>
2025-10-13 14:55:53 +0300

    Accounts: Modify user model

--- accounts/src/main/java/ru/yandex/practicum/accounts/model/User.java
+++ accounts/src/main/java/ru/yandex/practicum/accounts/model/User.java
@@ -1,19 +1,42 @@
 package ru.yandex.practicum.accounts.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Data
 @Table("users")
+@Data
 public class User {
+
     @Id
     private Long id;
+
+    @Column("username")
     private String username;
+
+    @Column("password")
     private String password;
+
+    @Column("name")
+    private String name;
+
+    @Column("email")
+    private String email;
+
+    @Column("birthdate")
+    private LocalDate birthdate;
+
+    @Column("role")
     private String role;
+
+    @Column("enabled")
     private Boolean enabled;
+
+    @Column("created_at")
     private LocalDateTime createdAt;
 }
