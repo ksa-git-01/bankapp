1abb78a4cb6ef3130a8482ff4947d986e719cb2a
ksa <krutikov.s@gmail.com>
2025-10-17 12:40:16 +0300

    Blocker: Add SecurityConfig

---
+++ blocker/src/main/java/ru/yandex/practicum/blocker/configuration/CustomGrantedAuthoritiesConverter.java
@@ -0,0 +1,46 @@
+package ru.yandex.practicum.blocker.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+public class CustomGrantedAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+
+        if (resourceAccess == null) {
+            log.warn("No resource_access claim in JWT token");
+            return authorities;
+        }
+
+        for (Map.Entry<String, Object> entry : resourceAccess.entrySet()) {
+            if (entry.getValue() instanceof Map) {
+                @SuppressWarnings("unchecked")
+                Map<String, Object> clientResource = (Map<String, Object>) entry.getValue();
+
+                if (clientResource.containsKey("roles") && clientResource.get("roles") instanceof List) {
+                    @SuppressWarnings("unchecked")
+                    List<String> roles = (List<String>) clientResource.get("roles");
+
+                    for (String role : roles) {
+                        authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+                    }
+                }
+            }
+        }
+
+        return authorities;
+    }
+}
