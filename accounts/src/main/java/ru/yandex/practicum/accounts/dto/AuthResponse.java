04315b846eed32c95f4f258f9ba88c93385b5af4
ksa <krutikov.s@gmail.com>
2025-10-17 12:48:17 +0300

    Accounts: Convert auth response to record

--- accounts/src/main/java/ru/yandex/practicum/accounts/dto/AuthResponse.java
+++ accounts/src/main/java/ru/yandex/practicum/accounts/dto/AuthResponse.java
@@ -1,16 +1,4 @@
 package ru.yandex.practicum.accounts.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@AllArgsConstructor
-@Getter
-@Setter
-public class AuthResponse {
-    private String token;
-    private String type;
-    private Long userId;
-    private String username;
-    private String role;
+public record AuthResponse(String token, String type, Long userId, String username, String role) {
 }
